4918cfe8016f799e9bcfeb61f2d23756010379a8
kyohei1157 <71009388+kyohei1157@users.noreply.github.com>
2020-12-13 16:46:00 +0900

    Update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,12 +11,17 @@ RSpec.describe User, type: :model do
     it "emailが空では登録できない" do
       user = FactoryBot.build(:user)
       user.email = ""
+      user.email = "sample123gmail.com"
       user.valid?
       expect(user.errors.full_messages).to include("Email can't be blank")
     end
     it "passwordが空では登録できない" do
       user = FactoryBot.build(:user)
       user.password = ""
+      user.password = "aaaaaa"
+      user.password = "123456"
+      user.password = "ａａａ１２３"
+      user.password = "aaa12"
       user.valid?
       expect(user.errors.full_messages).to include("Password can't be blank")
     end
@@ -29,24 +34,28 @@ RSpec.describe User, type: :model do
     it "surnameが空では登録できない" do
       user = FactoryBot.build(:user)
       user.surname = ""
+      user.surname = "123"
       user.valid?
       expect(user.errors.full_messages).to include("Password_confirmation can't be blank")
     end
     it "nameが空では登録できない" do
       user = FactoryBot.build(:user)
       user.name = ""
+      user.name = "456"
       user.valid?
       expect(user.errors.full_messages).to include("Password_confirmation can't be blank")
     end
     it "surname_firiganaが空では登録できない" do
       user = FactoryBot.build(:user)
       user.surname_firigana = ""
+      user.surname_firigana = "あいうえお"
       user.valid?
       expect(user.errors.full_messages).to include("Password_confirmation can't be blank")
     end
     it "name_firiganaが空では登録できない" do
       user = FactoryBot.build(:user)
       user.name_firigana = ""
+      user.name_firigana = "かきくけこ"
       user.valid?
       expect(user.errors.full_messages).to include("Password_confirmation can't be blank")
     end
