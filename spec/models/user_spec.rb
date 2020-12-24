76cb59fa04ef7f0e640e4be754f0e987e1422795
kyohei1157 <71009388+kyohei1157@users.noreply.github.com>
2020-12-24 16:38:31 +0900

    Update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe 'ユーザー新規登録' do
+  before do
     it "nicknameが空だと登録できない" do
       @user = FactoryBot.build(:user)
       @user.nickname = ""
