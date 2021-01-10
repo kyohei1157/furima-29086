c1685c00781463e8b4a99dd4372c739312cdb462
kyohei1157 <71009388+kyohei1157@users.noreply.github.com>
2021-01-10 16:38:00 +0900

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,47 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_23_120039) do
-
-  create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.string "category", null: false
-    t.string "status", null: false
-    t.string "shipping_fee_burden", null: false
-    t.string "shipping_area", null: false
-    t.date "days_to_ship", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.string "category", null: false
-    t.string "status", null: false
-    t.string "shipping_fee_burden", null: false
-    t.string "shipping_area", null: false
-    t.date "days_to_ship", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  # create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-  #   t.string "category", null: false
-  #   t.string "status", null: false
-  #   t.string "shipping_fee_burden", null: false
-  #   t.string "shipping_area", null: false
-  #   t.date "days_to_ship", null: false
-  #   t.datetime "created_at", precision: 6, null: false
-  #   t.datetime "updated_at", precision: 6, null: false
-  # end
-
-  # create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-  #   t.string "category", null: false
-  #   t.string "status", null: false
-  #   t.string "shipping_fee_burden", null: false
-  #   t.string "shipping_area", null: false
-  #   t.date "days_to_ship", null: false
-  #   t.datetime "created_at", precision: 6, null: false
-  #   t.datetime "updated_at", precision: 6, null: false
-  # end
+ActiveRecord::Schema.define(version: 2020_11_25_082245) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "surname", default: "", null: false
