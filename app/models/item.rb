9bef96914225f4f176731ebb8d4c38d87aa2ae6c
kyohei1157 <71009388+kyohei1157@users.noreply.github.com>
2021-01-24 19:35:11 +0900

    バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,12 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  validates :name, presence: true
+  validates :explanation, presence: true
+  validates :category_id, presence: true
+  validates :status_id, presence: true
+  validates :shipping_fee_burden_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :days_to_ship_id, presence: true
+  validates :price, presence: true
+  validates :image, presence: true
 end
