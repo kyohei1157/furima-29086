a97a107ce88f22664a7be281ebcf73731b2e29be
kyohei1157 <71009388+kyohei1157@users.noreply.github.com>
2021-01-27 21:11:33 +0900

    セレクトボックス

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,22 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  validates :title, :text, presence: true
+  validates :category_id, numericality: { other_than: 1 }
+  belongs_to :status
+  validates :title, :text, presence: true
+  validates :status_id, numericality: { other_than: 1 }
+  belongs_to :shipping_fee_burden
+  validates :title, :text, presence: true
+  validates :shipping_fee_burden_id, numericality: { other_than: 1 }
+  belongs_to :prefecture
+  validates :title, :text, presence: true
+  validates :prefecture_id, numericality: { other_than: 1 }
+  belongs_to :days_to_ship
+  validates :title, :text, presence: true
+  validates :days_to_ship_id, numericality: { other_than: 1 }
+  belongs_to :user
+  has_one :buy_record
   has_one_attached :image
   validates :name, presence: true
   validates :explanation, presence: true
