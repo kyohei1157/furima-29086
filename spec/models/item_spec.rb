a016c6534678b7caa9b1097c911430c5194ea6d0
kyohei1157 <71009388+kyohei1157@users.noreply.github.com>
2021-01-30 14:15:08 +0900

    commit

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -61,19 +61,35 @@ RSpec.describe Item, type: :model do
       @item.valid?
       expect(@item.errors.full_messages).to include("Price is invalid. Input half-width characters.")
     end
-    it "1の場合登録できない" do
-      @item.category_id = 1
-      @item.status_id = 1
-      @item.shipping_fee_burden_id = 1
-      @item.prefecture_id = 1
-      @item.days_to_ship_id = 1
-      @item.valid?
-      expect(@item.errors.full_messages).to include("Category must be other than 1", "Status must be other than 1", "Shipping fee burden must be other than 1", "Prefecture must be other than 1", "Days to ship must be other than 1")
-    end
     it "価格が¥300~¥9,999,999ではない場合登録できない" do
       @item.price = 200
       @item.valid?
       expect(@item.errors.full_messages).to include("Price is not included in the list")
+    end
+    it "category_idが1の場合登録できない" do
+      @item.category_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category must be other than 1" )
+    end
+    it "status_idが1の場合登録できない" do
+      @item.status_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Status must be other than 1" )
+    end
+    it "shipping_fee_burden_idが1の場合登録できない" do
+      @item.shipping_fee_burden_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Shipping fee burden must be other than 1" )
+    end
+    it "prefecture_idが1の場合登録できない" do
+      @item.prefecture_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Prefecture must be other than 1" )
+    end
+    it "days_to_ship_idが1の場合登録できない" do
+      @item.days_to_ship_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Days to ship must be other than 1" )
     end
   end
 end
